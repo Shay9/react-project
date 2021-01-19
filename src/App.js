9f70c96febe8b4716d4dcbd7c17bd3bd611e38e3
Shay9 <72321250+Shay9@users.noreply.github.com>
2021-01-18 19:21:24 -0500

    git repository url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <h1>
       Weather App
+      </h1>
+      <footer>
+        This project was coded by <a href="https://adoring-meninsky-e54200.netlify.app/"
+        target="_blank"> Shadea Nance  </a> and is
+      <a href="https://github.com/Shay9/react-project"
+      target="_blank"
+      >
+          {" "} open-sourced on GitHib
+      </a>
+      </footer>
     </div>
   );
 }
